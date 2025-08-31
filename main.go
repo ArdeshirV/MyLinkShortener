a62a0203950e499dd5dd9b995b0c4663190d6237
ArdeshirV <e-job@protonmail.com>
2025-08-31 17:40:52 +0200

    Add global constants and variables

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+    "log/slog"
+    "math/rand"
+    "net/http"
 )
 
 func main() {
 	fmt.Println(Prompt("My Link Shortener"))
 }
+
+const (
+    baseURL = "http://localhost:8080"
+    shortURLLen = 4
+)
+
+var (
+    urlMap = make(map[string]string)
+    letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+)
+
+

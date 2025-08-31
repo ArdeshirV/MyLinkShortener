e8accdb2a50179baf9d231468ebf9ce334e230f6
ArdeshirV <e-job@protonmail.com>
2025-08-31 17:44:12 +0200

    Add redirectHandler

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"fmt"
-    "log/slog"
-    "math/rand"
-    "net/http"
+	"log/slog"
+	"math/rand"
+	"net/http"
 )
 
 func main() {
@@ -12,13 +12,20 @@ func main() {
 }
 
 const (
-    baseURL = "http://localhost:8080"
-    shortURLLen = 4
+	baseURL     = "http://localhost:8080"
+	shortURLLen = 4
 )
 
 var (
-    urlMap = make(map[string]string)
-    letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	urlMap  = make(map[string]string)
+	letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
 )
 
-
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
+    shortURL := r.URL.Path[1:]
+    if url, ok := urlMap[shortURL]; ok {
+        http.Redirect(w, r, url, http.StatusFound)
+    } else {
+        http.NotFound(w, r)
+    }
+}
